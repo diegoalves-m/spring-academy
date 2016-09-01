864dcc65650258e6ca65b0fb945a7a598dc656a7
Diego Alves <d14l.diego@gmail.com>
2016-09-01 14:46:02 -0300

    little changes

---
+++ src/main/java/com/algaworks/wine/config/WebConfig.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.algaworks.wine.config;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * @author Diego Alves
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+    
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+         return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"));
+            }
+        };
+    }
+    
+}
